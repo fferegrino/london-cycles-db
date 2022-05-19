68929c8d91bdffa8a19425177a52021dc6b0022d
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-05-19 21:12:32 +0100

    Add station info download

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from datetime import datetime
 from pathlib import Path
 
@@ -14,25 +15,13 @@ def get_number(additional_properties, key):
     return int(nb)
 
 
-def get_stations():
-    all_bike_points = bike_point.all()
+def get_stations(all_bike_points):
     data = []
-
     for place in all_bike_points:
         bikes = get_number(place.additionalProperties, "NbBikes")
         empty_docks = get_number(place.additionalProperties, "NbEmptyDocks")
         docks = get_number(place.additionalProperties, "NbDocks")
-        data.append(
-            (
-                execution_time.isoformat(),
-                place.id,
-                place.lat,
-                place.lon,
-                bikes,
-                empty_docks,
-                docks,
-            )
-        )
+        data.append((execution_time.isoformat(), place.id, place.lat, place.lon, bikes, empty_docks, docks,))
 
     return data
 
@@ -40,11 +29,33 @@ def get_stations():
 headers = ["query_time", "place_id", "lat", "lon", "bikes", "empty_docks", "docks"]
 
 
-write_headers = not csv_file.exists()
+first_file_of_the_day = not csv_file.exists()
 with open(csv_file, "a") as w:
     writer = csv.writer(w)
-    if write_headers:
+    if first_file_of_the_day:
         writer.writerow(headers)
-
-    for station_row in get_stations():
+    bike_points = bike_point.all()
+    for station_row in get_stations(bike_points):
         writer.writerow(station_row)
+
+
+if first_file_of_the_day:
+    information_file = Path("data", f"stations-{execution_time.strftime('%Y-%m-%d')}.csv")
+    # This is very inefficient, needs refactoring.
+    dictionaries = []
+    properties = set()
+    pattern = re.compile(r"(?<!^)(?=[A-Z])")
+    for bike_point in bike_points:
+        props = {
+            pattern.sub("_", prop.key).lower(): prop.value
+            for prop in bike_point.additionalProperties
+            if not prop.key.startswith("Nb")
+        }
+        station_dict = {"common_name": bike_point.commonName, "place_id": bike_point.id, **props}
+        properties.update(station_dict.keys())
+        dictionaries.append(station_dict)
+    with open(information_file, "w") as w:
+        writer = csv.DictWriter(w, fieldnames=list(set(properties)))
+        writer.writeheader()
+        for dd in dictionaries:
+            writer.writerow(dd)
